7d60bcccd02625e5d165ca308daeb4af77410101
evilgeniusatwork <dave.andsager86@gmail.com>
2021-12-31 14:45:30 -0600

    button testing

--- app/src/main/java/com/example/testandroid/MainActivity.java
+++ app/src/main/java/com/example/testandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.testandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateText(View view) {
+        TextView tv1 = (TextView)findViewById(R.id.movement_text);
+        tv1.setText("Clicking? Yes");
     }
 }
